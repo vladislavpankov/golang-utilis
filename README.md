1f131f7daaa202bf95632979239a5ff0bcc635fe
vladislavpankov <pankovszzz29@gmail.com>
2025-10-15 12:37:15 +0700

    changed README and updates

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-getting used to golang
-:)
+just writing my own utilis in go to get used to the lang
+and to get a better understanding of linux in general
---
+++ dup/dup1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main() {
+	count := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		count[input.Text()]++
+	}
+
+	for line, n := range count {
+		if n > 1 {
+			fmt.Printf("%d\t %s\n", n, line)
+		}
+	}
+}
